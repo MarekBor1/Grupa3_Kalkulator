03ff1a4d7ccf7736d0f3b048a2896c62df494f17
MarcinRyznar <92849294+MarcinRyznar@users.noreply.github.com>
2022-05-04 23:36:28 +0200

    Update kalkulator_projekt.py

--- kalkulator_projekt.py
+++ kalkulator_projekt.py
@@ -2,6 +2,39 @@ from tkinter import *
 import math
 
 
+class Liczby:
+    def __init__(self, x_axis, y_axis):
+        self.czesc_rzeczywista = x_axis
+        self.czesc_urojona = y_axis
+
+    def dostac_czesc_rzeczywista(self):
+        return self.czesc_rzeczywista
+
+    def dostac_czesc_urojona(self):
+        return self.czesc_urojona
+
+    def ustaw_czesc_rzeczywista(self, parametr):
+        self.czesc_rzeczywista = parametr
+
+    def ustaw_czesc_urojona(self, parametr):
+        self.czesc_urojona = parametr
+
+    def modul(self):
+        return math.sqrt(self.czesc_rzeczywista * 2 + self.czesc_urojona * 2)
+
+    def set_both_parts(self, parametr_x, parametr_y):
+        self.ustaw_czesc_rzeczywista(parametr_x)
+        self.ustaw_czesc_urojona(parametr_y)
+
+    def get_angle(self):
+        if self.modul() != 0:
+            return math.acos(self.czesc_rzeczywista / self.modul())
+        else:
+            return 0
+
+    def ustaw_modul_katowy(self, kat, modul):
+        self.czesc_rzeczywista = math.cos(kat) * modul
+        self.czesc_urojona = math.sin(kat) * modul
 
 
 class Operation:
